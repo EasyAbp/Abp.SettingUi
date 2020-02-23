ced295f7c28af59687e34de93db3f7101d4b9c8b
wakuflair <wakuflair@163.com>
2020-02-23 23:11:25 +0800

    Nuget packages configuration

---
+++ nupkg/common.ps1
@@ -0,0 +1,13 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$rootFolder = Join-Path $packFolder "../"
+
+# List of projects
+$projects = (
+
+    "src/EasyAbp.Abp.SettingUi.Application",
+    "src/EasyAbp.Abp.SettingUi.Application.Contracts",
+    "src/EasyAbp.Abp.SettingUi.Domain",
+    "src/EasyAbp.Abp.SettingUi.Domain.Shared",
+    "src/EasyAbp.Abp.SettingUi.Web"
+)
