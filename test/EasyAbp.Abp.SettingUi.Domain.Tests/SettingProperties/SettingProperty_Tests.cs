fb8cc27219358f152d12c9353ed0a5fe175315ee
wakuflair <wakuflair@163.com>
2020-06-18 14:13:49 +0800

    * Adjust setting properties * Add localization items

--- test/EasyAbp.Abp.SettingUi.Domain.Tests/SettingProperties/SettingProperty_Tests.cs
+++ test/EasyAbp.Abp.SettingUi.Domain.Tests/SettingProperties/SettingProperty_Tests.cs
@@ -6,16 +6,26 @@ using Volo.Abp.Json;
 using Volo.Abp.Settings;
 using Volo.Abp.VirtualFileSystem;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace EasyAbp.Abp.SettingUi.SettingProperties
 {
     public class SettingProperty_Tests : SettingUiDomainTestBase
     {
+        private readonly ITestOutputHelper _output;
+        private readonly ISettingDefinitionManager _settingDefinitionManager;
+
+        public SettingProperty_Tests(ITestOutputHelper output)
+        {
+            _output = output;
+            _settingDefinitionManager = GetRequiredService<ISettingDefinitionManager>();
+        }
+
         [Fact]
         public Task BuiltIn_Setting_Properties_Should_Be_Defined()
         {
             // Arrange
-            var settingDefinitions = GetRequiredService<ISettingDefinitionManager>().GetAll();
+            var settingDefinitions = _settingDefinitionManager.GetAll();
             var fileInfo = GetRequiredService<IVirtualFileProvider>().GetFileInfo("/SettingProperties/AbpSettingUiSettingProperties.json");
             var jsonSerializer = GetRequiredService<IJsonSerializer>();
 
@@ -25,6 +35,7 @@ namespace EasyAbp.Abp.SettingUi.SettingProperties
             // Assert
             foreach (var settingDefinition in settingDefinitions)
             {
+                _output.WriteLine($"Check property of the setting {settingDefinition.Name}");
                 propertyDict.ShouldContainKey(settingDefinition.Name, $"Setting property not found, setting name: {settingDefinition.Name}");
             }
 
