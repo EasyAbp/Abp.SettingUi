1df54c159b42d45e0f6a30ab3b48602ccc999d11
Cihan Güllü <cihangullu54@gmail.com>
2020-12-24 23:24:38 +0300

    Sample usage added.

--- sample/MyAbpApp/src/MyAbpApp.Application.Contracts/Permissions/MyAbpAppPermissions.cs
+++ sample/MyAbpApp/src/MyAbpApp.Application.Contracts/Permissions/MyAbpAppPermissions.cs
@@ -1,10 +1,23 @@
-﻿namespace MyAbpApp.Permissions
-{
-    public static class MyAbpAppPermissions
-    {
-        public const string GroupName = "MyAbpApp";
+﻿using EasyAbp.Abp.SettingUi.Authorization;
 
-        //Add your own permission names. Example:
-        //public const string MyPermission1 = GroupName + ".MyPermission1";
-    }
+namespace MyAbpApp.Permissions
+{
+	public static class MyAbpAppPermissions
+	{
+		public const string GroupName = "MyAbpApp";
+
+		//Add your own permission names. Example:
+		//public const string MyPermission1 = GroupName + ".MyPermission1";
+	}
+
+	public static class System
+	{
+		public const string Default = SettingUiPermissions.GroupName + ".System";
+		public static class Password
+		{
+			public const string Group2 = Default + ".Password";
+			public const string RequiredLength = Group2 + ".Abp.Identity.Password.RequiredLength";
+			public const string RequiredUniqueChars = Group2 + ".Abp.Identity.Password.RequiredUniqueChars";
+		}
+	}
 }
