6dea0875a572ea530c49c0004332ee2c0bf67962
Cihan Güllü <cihangullu54@gmail.com>
2020-12-24 23:09:14 +0300

    permission checks added.

--- src/EasyAbp.Abp.SettingUi.Web/Pages/Components/SettingViewComponent.cs
+++ src/EasyAbp.Abp.SettingUi.Web/Pages/Components/SettingViewComponent.cs
@@ -1,20 +1,63 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using EasyAbp.Abp.SettingUi.Authorization;
 using EasyAbp.Abp.SettingUi.Dto;
 using EasyAbp.Abp.SettingUi.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+using Volo.Abp.Authorization.Permissions;
 
 namespace EasyAbp.Abp.SettingUi.Web.Pages.Components
 {
     [Widget(StyleFiles = new[] {"/Pages/Components/Default.css"})]
     public class SettingViewComponent : AbpViewComponent
     {
-        public IViewComponentResult Invoke(SettingGroup parameter)
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IPermissionDefinitionManager _permissionDefinitionManager;
+
+        public SettingViewComponent(IAuthorizationService authorizationService, IPermissionDefinitionManager permissionDefinitionManager)
         {
-            var settingInfos = parameter.SettingInfos.Select(si => new SettingHtmlInfo(si));
-            return View("~/Pages/Components/Default.cshtml", settingInfos);
+            _authorizationService = authorizationService;
+            _permissionDefinitionManager = permissionDefinitionManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(SettingGroup parameter)
+        {
+            var definedSettingUiPermissions = _permissionDefinitionManager.GetPermissions().Where(p => p.Name.StartsWith(SettingUiPermissions.GroupName));
+
+            var settingInfoGroups = parameter.SettingInfos
+                .GroupBy(sd => sd.Properties[SettingUiConst.Group2].ToString())
+                .Select(grp => new
+                {
+                    Permission = $"{SettingUiPermissions.GroupName}.{grp.FirstOrDefault()?.Properties[SettingUiConst.Group1]}.{grp.Key}",
+                    SettingInfoList = grp.ToList()
+                });
+
+            var settingHtmlInfoList = new List<SettingHtmlInfo>();
+            foreach (var settingInfoGroup in settingInfoGroups)
+            {
+                var definedSettingUiGroupPermission = definedSettingUiPermissions.FirstOrDefault(p => p.Name == settingInfoGroup.Permission);
+                if (definedSettingUiGroupPermission == null || await _authorizationService.IsGrantedAsync(definedSettingUiGroupPermission.Name))
+                {
+                    settingHtmlInfoList.AddRange(
+                        settingInfoGroup.SettingInfoList
+                            .Where(si => string.IsNullOrEmpty(si.Permission))
+                            .Select(si => new SettingHtmlInfo(si))
+                        );
+                    foreach (var settingInfo in settingInfoGroup.SettingInfoList.Where(si => !string.IsNullOrEmpty(si.Permission)))
+                    {
+                        if (await _authorizationService.IsGrantedAsync(settingInfo.Permission))
+                        {
+                            settingHtmlInfoList.Add(new SettingHtmlInfo(settingInfo));
+                        }
+                    }
+                }
+            }
+
+            return View("~/Pages/Components/Default.cshtml", settingHtmlInfoList);
         }
     }
 
--- src/EasyAbp.Abp.SettingUi.Web/Pages/SettingUiPageContributor.cs
+++ src/EasyAbp.Abp.SettingUi.Web/Pages/SettingUiPageContributor.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using EasyAbp.Abp.SettingUi.Authorization;
 using EasyAbp.Abp.SettingUi.Web.Pages.Components;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.SettingManagement.Web.Pages.SettingManagement;
 
 namespace EasyAbp.Abp.SettingUi.Web.Pages
@@ -12,10 +14,16 @@ namespace EasyAbp.Abp.SettingUi.Web.Pages
         public async Task ConfigureAsync(SettingPageCreationContext context)
         {
             var settingUiAppService = context.ServiceProvider.GetService<ISettingUiAppService>();
-
+            var authorizationService = context.ServiceProvider.GetRequiredService<IAuthorizationService>();
+            var permissionDefinitionManager = context.ServiceProvider.GetRequiredService<IPermissionDefinitionManager>();
+            var definedSettingUiPermissions = permissionDefinitionManager.GetPermissions().Where(p => p.Name.StartsWith(SettingUiPermissions.GroupName));
             foreach (var grp in await settingUiAppService.GroupSettingDefinitions())
             {
-                context.Groups.Add(new SettingPageGroup(grp.GroupName, grp.GroupDisplayName, typeof(SettingViewComponent), grp));
+                var definedSettingUiGroupPermission = definedSettingUiPermissions.FirstOrDefault(p => p.Name == grp.Permission);
+                if (definedSettingUiGroupPermission == null || await authorizationService.IsGrantedAsync(definedSettingUiGroupPermission.Name))
+                {
+                    context.Groups.Add(new SettingPageGroup(grp.GroupName, grp.GroupDisplayName, typeof(SettingViewComponent), grp));
+                }
             }
         }
 
