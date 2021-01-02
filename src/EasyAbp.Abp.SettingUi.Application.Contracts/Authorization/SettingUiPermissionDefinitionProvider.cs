ed28b19a903a3c4932703192f64e6c48c5370d81
wakuflair <wakuflair@163.com>
2021-01-02 19:49:54 +0800

    Simplify permissions

--- src/EasyAbp.Abp.SettingUi.Application.Contracts/Authorization/SettingUiPermissionDefinitionProvider.cs
+++ src/EasyAbp.Abp.SettingUi.Application.Contracts/Authorization/SettingUiPermissionDefinitionProvider.cs
@@ -12,9 +12,6 @@ namespace EasyAbp.Abp.SettingUi.Authorization
         {
             var moduleGroup = context.AddGroup(SettingUiPermissions.GroupName, L("Permission:SettingUi"));
             var showSettingPagePermission = moduleGroup.AddPermission(SettingUiPermissions.ShowSettingPage, L("Permission:SettingUi.ShowSettingPage"));
-            showSettingPagePermission.AddChild(SettingUiPermissions.Host.Default, L("Permission:SettingUi.Host"), MultiTenancySides.Host);
-            showSettingPagePermission.AddChild(SettingUiPermissions.Tenant.Default, L("Permission:SettingUi.Tenant"));
-            showSettingPagePermission.AddChild(SettingUiPermissions.User.Default, L("Permission:SettingUi.User"));
         }
 
         private static LocalizableString L(string name)
--- src/EasyAbp.Abp.SettingUi.Application.Contracts/Authorization/SettingUiPermissions.cs
+++ src/EasyAbp.Abp.SettingUi.Application.Contracts/Authorization/SettingUiPermissions.cs
@@ -7,21 +7,6 @@ namespace EasyAbp.Abp.SettingUi.Authorization
         public const string GroupName = "SettingUi";
         public const string ShowSettingPage = GroupName + ".ShowSettingPage";
 
-        public static class Tenant
-        {
-            public const string Default = GroupName + ".Tenant";
-        }
-
-        public static class User
-        {
-            public const string Default = GroupName + ".User";
-        }
-
-        public static class Host
-        {
-            public const string Default = GroupName + ".Host";
-        }
-
         public static string[] GetAll()
         {
             return ReflectionHelper.GetPublicConstantsRecursively(typeof(SettingUiPermissions));
