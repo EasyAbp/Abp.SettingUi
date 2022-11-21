31079d423841fd37f7ca4a70807ba848a2e634af
blackWinds <745673576@qq.com>
2022-11-21 17:52:15 +0800

    fix ver.6.0 save error

--- src/EasyAbp.Abp.SettingUi.Web/Pages/SettingManagement/SettingUi/Index.js
+++ src/EasyAbp.Abp.SettingUi.Web/Pages/SettingManagement/SettingUi/Index.js
@@ -3,48 +3,69 @@
     var service = easyAbp.abp.settingUi.settingUi;
     var l = abp.localization.getResource("EasyAbpAbpSettingUi");
 
-    $("form").submit(function (e) {
-        e.preventDefault();
-
-        // Skip abp `EmailSettingsForm`
-        if (e.currentTarget.id == 'EmailSettingsForm') {
-            return;
-        }
-
-        if (!$(e.currentTarget).valid()) {
-            return;
-        }
-
-        var input = $(e.currentTarget).serializeFormToObject();
-        service.setSettingValues(input)
-            .then(function (result) {
-                //abp.notify.success(l("SuccessfullySaved"));
-                $(document).trigger("AbpSettingSaved");
-            });
-    });
-
-    $(".reset").click(function (e) {
-        var form = e.currentTarget.closest("form");
-        abp.message.confirm(
-            l("ResetConfirm", $(form).find("h4").text()),
-            function (result) {
-                if (result) {
-                    var input = $(form)
-                        .find(":input[id]")
-                        .map(function() {return this.id;})
-                        .get();
-                    service.resetSettingValues(input)
-                        .then(function (result) {
-                            // get the index of current selected tab
-                            var index = $("a.nav-link.active").parents().index()
-                            location.href = "#" + index;
-                            location.reload();
-                        });
-                }
+    var event_fn = function (id = '') {
+        $(id + "form").submit(function (e) {
+            e.preventDefault();
+            // Skip abp `EmailSettingsForm`
+            if (e.currentTarget.id == 'EmailSettingsForm') {
+                return;
             }
-        );
-    })
 
+            if (!$(e.currentTarget).valid()) {
+                return;
+            }
+
+            var input = $(e.currentTarget).serializeFormToObject();
+            service.setSettingValues(input)
+                .then(function (result) {
+                    //abp.notify.success(l("SuccessfullySaved"));
+                    $(document).trigger("AbpSettingSaved");
+                });
+        });
+
+        $(id + ".reset").click(function (e) {
+            var form = e.currentTarget.closest("form");
+            abp.message.confirm(
+                l("ResetConfirm", $(form).find("h4").text()),
+                function (result) {
+                    if (result) {
+                        var input = $(form)
+                            .find(":input[id]")
+                            .map(function () { return this.id; })
+                            .get();
+                        service.resetSettingValues(input)
+                            .then(function (result) {
+                                // get the index of current selected tab
+                                var index = $("a.nav-link.active").parents().index()
+                                location.href = "#" + index;
+                                location.reload();
+                            });
+                    }
+                }
+            );
+        })
+    }
+
+    if ($('.nav-item').parent().attr('id') == 'tabs-nav') {
+        //Support versions after 6.0 binding
+        $('#tabs-nav .nav-item .nav-link').click(function () {
+            var _this = $(this);
+            if (_this.attr('data-bs-target') !== undefined) {
+                return;
+            }
+            var btn_set = setInterval(function () {
+                var _id = '#' + _this.data('id') + " ";
+                event_fn(_id);
+                if ($(_id + " form").length > 0) {
+                    clearInterval(btn_set);
+                }
+            }, 200);
+        });
+    }
+    else {
+        //Support versions before 6.0 binding
+        event_fn();
+    }
 
     if (location.hash) {
         var index = location.hash.substring(1);
